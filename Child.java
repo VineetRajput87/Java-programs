22f0acba6bac3213e9a7911965cf7b37677deed6
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2022-12-19 11:51:27 +0530

    Add files via upload

---
+++ Child.java
@@ -0,0 +1,24 @@
+class Base
+{
+    void show()
+    {
+        System.out.println("Base Class");
+    }
+}
+class Child extends Base
+{
+    void show()
+    {
+        System.out.println("Child Class\n");
+    }
+    public static void main(String[] args) 
+    {
+        Base r;
+        Base ob = new Base();
+        Child obj = new Child();
+        r=ob;
+        r.show();
+        r=obj;
+        r.show();
+    }
+}
