8fbefa3b03b7c9ce209c108d002fc274ad453138
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2022-12-22 23:20:25 +0530

    Add files via upload

---
+++ GreaterNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class GreaterNumber
+{
+    int a,b;
+    void compare()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Value of A : ");
+        a=sc.nextInt();
+        System.out.println("Enter Value of B : ");
+        b=sc.nextInt();
+        if(a>b)
+           System.out.println("Greater Value is : "+a);
+        else if(b>a)
+           System.out.println("Greater Value is : "+b);
+        else
+           System.out.println("Both values are Equal");
+    }
+    
+    public static void main(String[] args) 
+    {
+        GreaterNumber obj = new GreaterNumber();
+        obj.compare();
+    }
+}
