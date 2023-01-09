25d9de87250c9bd0dec1e81de8334967cfa9788c
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-01-09 23:18:45 +0530

    Create AddTwoNumbers.java

---
+++ AddTwoNumbers.java
@@ -0,0 +1,11 @@
+class Add
+{
+    int a=10;
+    int b=20;
+    
+    public static void main(String[] args)
+    {
+        Add obj = new Add();
+        System.out.println(obj.a+obj.b);
+    }
+}
