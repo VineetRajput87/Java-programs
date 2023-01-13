8b32340bb4d5b94a2dc8f6c97152c92879875960
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-01-13 22:03:16 +0530

    Create FillTheBucket.java

---
+++ FillTheBucket.java
@@ -0,0 +1,23 @@
+// Q.:- Vineet has a bucket having a capacity of K liters. It is already filled with X liters of water.
+
+// Find the maximum amount of extra water in liters that Vineet
+// can fill in the bucket without overflowing.
+
+import java.util.Scanner;
+
+class Vineet
+{
+	public static void main (String[] args)
+	{
+		int t;
+		Scanner sc= new Scanner(System.in);
+		t=sc.nextInt();
+		for(;t>0;t--)
+		{
+		    int a,b;
+		    a=sc.nextInt();
+		    b=sc.nextInt();
+		    System.out.println(a-b);
+		}
+	}
+}
