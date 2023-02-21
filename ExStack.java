2ce2abc33343bf1ebc3300323f917775b661bc8c
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-02-21 11:25:45 +0530

    Add files via upload

---
+++ ExStack.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+public class ExStack 
+{
+    public static void main(String[] args) 
+    {
+        Stack<String> obj = new Stack<String>();
+        
+        obj.push("Vineet");
+        obj.push("Kanha");                                    
+        obj.push("Tahir"); 
+        obj.push("Mohit"); 
+        obj.push("Ram");
+        System.out.println("The list is : "+obj); 
+        System.out.println("The size is : "+obj.size()); 
+        obj.pop();
+        System.out.println("The List After Pop Operation : "+obj); 
+        System.out.println("The Size After Pop Operation "+obj.size()); 
+
+    }
+}
---
+++ ExStack2.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.Stack;
+public class ExStack2 
+{
+    public static void main(String[] args) 
+    {
+        int choice;
+        Scanner sc= new Scanner(System.in);
+        Stack<String> obj = new Stack<String>();
+        do{
+        System.out.println("1.Push\n2.Pop\n3.Desplay\n4.Exit\n");
+        System.out.println("Enter Your Choice : ");
+        choice=sc.nextInt();
+        if(choice==1)
+        {
+            String a;
+            System.out.println("Enter a String : ");
+            a=sc.next();
+            obj.push(a);
+        }
+        else if(choice==2)
+        {
+            obj.pop();
+        }
+        else if(choice==3)
+        {
+            System.out.println(obj);
+        }
+        else if(choice==4)
+        continue;
+        else
+        {
+            System.out.println("WRONG INPUT\n PLEASE TRY AGAIN..!");
+        }
+    }while(choice!=4);
+    }
+}
