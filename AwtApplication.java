2ce4d26c405330db2a11d7f4c2331b2e8f95fd94
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-02-22 10:04:32 +0530

    Add files via upload

---
+++ AwtApplication.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+public class AwtApplication 
+{
+    public static void main(String arrp[])
+    {
+        Frame obj = new Frame("TITLE");
+        obj.setSize(400,400);
+        obj.setVisible(true);
+        Panel P = new Panel();
+        obj.add(P);
+        Label l1 = new Label("Enter First Name : ");
+        P.add(l1);
+        TextField textf1 = new TextField();
+        P.add(textf1);
+        Label l2 = new Label("Enter First Name : ");
+        P.add(l2);
+        TextField textf2 = new TextField();
+        P.add(textf2);
+        Label l3 = new Label("Enter First Name : ");
+        P.add(l3);
+        TextField textf3 = new TextField();
+        P.add(textf3);
+        
+
+    }
+}
