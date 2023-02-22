104453e1bd1fe2c28718bb6db4b8171b574bd2e4
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-02-22 10:04:45 +0530

    Add files via upload

---
+++ AwtFrame.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+public class AwtFrame 
+{
+    public static void main(String[] args) 
+    {
+        Frame obj = new Frame("The Title");
+        obj.setSize(400,400);
+        obj.setVisible(true);    
+        System.out.println("Vineet Rajput");
+    }
+}
