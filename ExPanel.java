0497c61cde9d6b9dec429a090ca39a4407a782ff
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-02-22 10:05:20 +0530

    Add files via upload

---
+++ ExPanel.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+public class ExPanel 
+{
+    public static void main(String[] args) 
+    {
+       Panel p = new Panel();
+       Frame obj = new Frame("The Title");
+       obj.setSize(400,400);
+       obj.setVisible(true);
+       obj.add(p);
+        
+        System.out.println("Vineet Rajput");
+    }
+}
