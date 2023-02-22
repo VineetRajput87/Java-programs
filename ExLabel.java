24b2b24b13ad3e2b2fd79112201d96fef4b42ec7
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-02-22 10:05:07 +0530

    Add files via upload

---
+++ ExLabel.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+public class ExLabel 
+{
+    public static void main(String[] args) 
+    {
+        //Panel p = new Panel();
+        Frame obj = new Frame("The Title");
+        obj.setSize(400,400);
+        obj.setVisible(true);
+        //Label lbl = new Label("Shree Radhe..!");
+        //obj.add(lbl);
+        // TextField ob = new TextField();
+        // obj.add(ob);      
+        TextArea A = new TextArea();
+        obj.add(A); 
+        System.out.println("Vineet Rajput");
+    }
+}
