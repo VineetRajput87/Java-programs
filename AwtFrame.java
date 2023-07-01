5ca2b1920f55220318262afaf3caee225a7d44c1
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-07-01 08:57:45 +0530

    Update AwtFrame.java

--- AwtFrame.java
+++ AwtFrame.java
@@ -4,7 +4,7 @@ public class AwtFrame
     public static void main(String[] args) 
     {
         Frame obj = new Frame("The Title");
-        obj.setSize(400,400);
+        obj.setSize(400,400); 
         obj.setVisible(true);    
         System.out.println("Vineet Rajput");
     }
