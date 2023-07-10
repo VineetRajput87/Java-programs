dc524fbb2869a3e1f659f35ef65c6882fd4ef80e
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-07-10 21:53:02 +0530

    Update WrapperExample1.java

--- WrapperExample1.java
+++ WrapperExample1.java
@@ -2,7 +2,7 @@ class WrapperExample1
 {
     public static void main(String arr[])
     {
-        int a=10;
+        int a=10; 
         Integer obj = Integer.valueOf(a); 
         Integer j=a;
         Integer r=obj;
