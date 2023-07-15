c158db128622d71f93744649879c0ef6c145ba59
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-07-15 21:24:17 +0530

    Update WrapperExample2.java

--- WrapperExample2.java
+++ WrapperExample2.java
@@ -2,7 +2,7 @@ public class WrapperExample2
 {
     public static void main(String[] args) 
     {
-        Integer a = new Integer(3);
+        Integer a = new Integer(3); 
         int i=a.intValue();
         int j=a;
         System.out.println(a+"\n"+i+"\n"+j);
