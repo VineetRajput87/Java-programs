f3288937464d680fc26c3d4d0585eadaba1a797c
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-07-26 20:29:04 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -6,7 +6,7 @@ public class Student
     float cgpa;
 
     void getData()
-    {
+    { 
         Scanner sc = new Scanner(System.in);
         System.out.print("Name : ");
         name =sc.next();
