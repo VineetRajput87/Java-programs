3bdb97c1b126b31afab56aca7e555c593592623c
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-08-05 21:00:49 +0530

    Update Sample3.java

--- Sample3.java
+++ Sample3.java
@@ -4,7 +4,7 @@ public class Sample3
     public static void main(String arr[])
     { 
        Sample3 obj =new Sample3();
-       obj.a=10;
+       obj.a=10; 
        obj.b=20;
        obj.c=30;
        System.out.println("The value of A : "+obj.a);
