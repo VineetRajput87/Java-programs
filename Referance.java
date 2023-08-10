66503a1d724386e9b99d26dc1eea98c723fbfdcf
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-08-10 21:17:25 +0530

    Update Referance.java

--- Referance.java
+++ Referance.java
@@ -3,7 +3,7 @@ public class Referance
     int A=10;
       public static void main(String sr[])
       { 
-        Referance r;
+        Referance r; 
         Referance obj = new Referance(); 
         r=obj;
         System.out.println("The value of A : "+r.A);
