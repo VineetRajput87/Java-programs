b5c38d2adcad1643900fd35d8f8dbf320fb30a6c
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-08-18 14:26:06 +0530

    Update IsItHotOrCold.java

--- IsItHotOrCold.java
+++ IsItHotOrCold.java
@@ -1,7 +1,7 @@
 // Q.:- Vineet considers the climate HOT if the temperature is above 20, otherwise he considers it COLD. You are given the temperature C, find whether the climate is HOT or COLD.
  
 import java.util.Scanner;
-
+  
 class Vineet
 {
 	public static void main (String[] args)
