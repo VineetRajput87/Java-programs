c088e7a740f0ae0066d02d1a65c08afdb1771311
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-09-05 21:58:53 +0530

    Update ExPanel.java

--- ExPanel.java
+++ ExPanel.java
@@ -1,7 +1,7 @@
 import java.awt.*;
 public class ExPanel 
 {
-    public static void main(String[] args) 
+    public static void main(String[] args)  
     {  
        Panel p = new Panel();
        Frame obj = new Frame("The Title");
