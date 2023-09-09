27af0d4d1e1ddcf6ec1547fd3cb551b94448e42e
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-09-09 20:56:08 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -3,7 +3,7 @@ public class Employee
 {
     int Id,salary;
     String firstname,lastname;  
-
+ 
     Employee(int Id,int salary,String firstname, String lastname)
     {
         //Scanner sc = new Scanner(System.in);
