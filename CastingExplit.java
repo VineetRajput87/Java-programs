fcfb1ef4d42e310df6b543b457f7578c3c11e801
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-09-14 22:20:04 +0530

    Update CastingExplit.java

--- CastingExplit.java
+++ CastingExplit.java
@@ -2,7 +2,7 @@ class CastingExplit
 {
     public static void main (String a[ ] )
     {
-         double c=10.000000;
+         double c=10.000000; 
          int b= (int)c;
          System.out.println("B : "+b);
     }
