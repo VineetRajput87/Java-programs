f8f3de2addf1a74b48d198005e292bd2bc24bc9f
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-09-21 20:40:36 +0530

    Update AwtFrame.java

--- AwtFrame.java
+++ AwtFrame.java
@@ -2,7 +2,7 @@ import java.awt.*;
 public class AwtFrame 
 {
     public static void main(String[] args)   
-    {
+    { 
         Frame obj = new Frame("The Title");
         obj.setSize(400,400); 
         obj.setVisible(true);    
